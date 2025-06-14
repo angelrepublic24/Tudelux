543e69f517e01b5e83fb5c8aeee55830bcc34ed3
Angel Almonte <angelrepublic24@gmail.com>
2025-06-14 12:48:39 -0400

    DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,20 +4,25 @@ import { redirect } from "next/navigation";
 // app/page.tsx o app/(admin)/page.tsx
 
 export default async function Page() {
-  const { user } = await verifySession();
-  console.log("Hola" + user);
+  try {
+    const { user } = await verifySession();
 
-  if (user?.roles?.includes("admin")) {
-    redirect("/admin");
+    if (user?.roles?.includes("admin")) {
+      redirect("/admin");
+    }
+
+    if (user?.roles?.includes("sales")) {
+      redirect("/sales");
+    }
+
+    if (user?.roles?.includes("distributor")) {
+      redirect("/distributor");
+    }
+
+    // No tiene roles válidos
+    redirect("/auth/login");
+  } catch (error) {
+    // 🔥 Aquí se atrapan errores del verifySession (como token inválido)
+    redirect("/auth/login");
   }
-
-  if (user?.roles?.includes("sales")) {
-    redirect("/sales");
-  }
-
-  if (user?.roles?.includes("distributor")) {
-    redirect("/distributor");
-  }
-
-  redirect("/auth/login");
 }
